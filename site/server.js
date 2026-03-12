c48de86cbc6a9ee23b4c9e694581756dd65677ea
ekksttasy <evaskaufman@gmail.com>
2026-03-12 12:49:33 +0000

    connected searchRoutes, added error handling

--- site/server.js
+++ site/server.js
@@ -28,6 +28,7 @@ pool.getConnection()
 
 const { initializeRouter } = require('./routes/search.js');
 const searchRoutes = initializeRouter(pool);
+app.use('/api', searchRoutes);
 
 // Homepage route
 app.get('/', (req, res) => {
@@ -43,6 +44,23 @@ app.get('/health', (req, res) => {
 });
 
 
+// Page not found
+app.use((req, res) => {
+    res.status(404).json({
+        error: 'Not Found',
+        message: `Cannot ${req.method} ${req.url}`
+    });
+});
+
+// Global error handling
+app.use((error, req, res, next) => {
+    console.error('Error:', error.stack);
+
+    res.status(500).json({
+        error: 'Internal Server Error',
+        message: error.message
+    });
+});
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
